f36dbd6b9f1167a39665390ab1b17d76d5654af3
Chamika Bandara <chamika.bandara@trycake.com>
2017-07-24 22:43:08 +0530

    Add changes

---
+++ frontend/src/main/frontend/src/app/hero.service.ts
@@ -0,0 +1,35 @@
+import { Injectable }    from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import { Hero } from './hero';
+
+@Injectable()
+export class HeroService {
+  private heroesUrl = "/api/heroes";  // URL to web api
+
+  constructor(private http: Http) { }
+
+  getHeroes(): Promise<Hero[]> {
+    return this.http.get(this.heroesUrl)
+      .toPromise()
+      .then(response => {
+        return response.json() as Hero[]
+      })
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+
+  getHero(id: number): Promise<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get(url)
+      .toPromise()
+      .then(response => response.json() as Hero)
+      .catch(this.handleError);
+  }
+}
