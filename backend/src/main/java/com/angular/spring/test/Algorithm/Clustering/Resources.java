305519ec54b25cd7b55b25b4b18441170cbcffb5
chamikabm <kasun.chamika@gmail.com>
2018-05-20 22:31:52 +0530

    Add changes to missing logics.

--- backend/src/main/java/com/angular/spring/test/Algorithm/Clustering/Resources.java
+++ backend/src/main/java/com/angular/spring/test/Algorithm/Clustering/Resources.java
@@ -1,6 +1,5 @@
 package com.angular.spring.test.Algorithm.Clustering;
 
-import com.angular.spring.test.manager.ClusterManager;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -20,6 +19,9 @@ public class Resources {
 
     static void init() {
 
+        String safeKcValue1 = "2-3-1-2-4";
+        String safeKcValue2 = "2-2-3-4-1";
+
         JSONParser parser = new JSONParser();
         Object obj;
         TreeMap<String, Object> initialValuesMap;
@@ -31,6 +33,10 @@ public class Resources {
             String kfName  = "StudentManagementRestApi";
             String kcValue = DEPTH + "-" + ALPHA + "-" + BETA + "-" + GAMMA + "-" + DELTA;
 
+            if (DEPTH <= 0 || DEPTH > 2) {
+                throw new RuntimeException("Invalid Architectural Constrains!");
+            }
+
             Object serviceObject = jsonObject.get(kfName);
             HashMap<String, Object> resultMap = (HashMap<String, Object>) serviceObject;
 
@@ -41,12 +47,38 @@ public class Resources {
                 initialValueWallet = initialValuesMap.get(initialValuesMap.firstKey());
                 rawValuesMap = (HashMap<String, HashMap<String, Double>>)initialValueWallet;
             } else {
+                if (isValueInTheRange(ALPHA) && isValueInTheRange(BETA) &&
+                        isValueInTheRange(GAMMA) && isValueInTheRange(DELTA)) {
+                    if(isValueInTheCoarseRange(BETA) && isValueInTheCoarseRange(GAMMA)) {
+                        kcValue = safeKcValue2;
+                    } else {
+                        kcValue = safeKcValue1;
+                    }
 
-                throw new RuntimeException("Invalid Architectural Constrains!");
+                    System.out.println(kcValue);
+                    if (resultMap.get(kcValue) != null) {
+                        clustersMap = (HashMap<String, Object>) resultMap.get(kcValue);
+                        initialValuesMap = new TreeMap<>(clustersMap);
+
+                        initialValueWallet = initialValuesMap.get(initialValuesMap.firstKey());
+                        rawValuesMap = (HashMap<String, HashMap<String, Double>>)initialValueWallet;
+                    } else {
+                        throw new RuntimeException("Invalid Architectural Constrains!");
+                    }
+                } else {
+                    throw new RuntimeException("Invalid Architectural Constrains!");
+                }
             }
-
         } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
+    }
+
+    private static boolean isValueInTheRange(int value) {
+        return (value > 0 && value < 5);
+    }
+
+    private static boolean isValueInTheCoarseRange(int value) {
+        return (value > 2 && value < 5);
     }
 }
